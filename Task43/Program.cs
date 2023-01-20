b77a45c3ca0bfcde49a64e62e4c6891a4cd46947
EkaterinaBildina <kukresch@gmail.com>
2023-01-20 14:11:44 +0300

    Task43 Program.cs success

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -6,6 +6,30 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Please input below coordinates:");
-double b1 = Convert.ToInt32(Console.ReadLine());
-double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input below data:");
+Console.Write("b1: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1: ");
+int numk1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2: ");
+int numb2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2: ");
+int numk2 = Convert.ToInt32(Console.ReadLine());
+
+
+double pointX = Math.Round(IntesectionPointX(numb1, numk1, numb2, numk2), 1, MidpointRounding.ToZero);
+double pointY = Math.Round(IntesectionPointY(numb1, numk1, numb2, numk2), 1, MidpointRounding.ToZero);
+Console.WriteLine($"b1 = {numb1}, k1 = {numk1}, b2 = {numb2}, k2 = {numk2} -> ( {pointX} ; {pointY} )");
+
+
+double IntesectionPointX(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double IntesectionPointY(double b1, double k1, double b2, double k2)
+{
+    double y = (k1 * b2 - k2 * b1) / (k1 - k2);
+    return y;
+}
